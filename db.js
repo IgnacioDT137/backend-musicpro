2bb2f2d48034d31bdcfd2003f15f31964d05a571
ignacio diaz <ign.diaz@duocuc.cl>
2023-05-16 19:16:36 -0400

    new db

--- db.js
+++ db.js
@@ -3,11 +3,13 @@ const mysql = require("mysql2");
 
 // creacion de la conexion a la base de datos
 const conexion = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "musicpro"
+    host: "aws.connect.psdb.cloud",
+    user: "20drxkckba91vvm8ljt8",
+    password: "pscale_pw_iHOYdXUr7HDwm7HnC9orhMQGbtbhu91v3YvQjhrOhvn",
+    database: "musicpro",
+    ssl: {
+        rejectUnauthorized: false
+    }
 }) 
 
 module.exports = conexion
